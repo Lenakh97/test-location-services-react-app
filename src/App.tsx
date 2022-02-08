4c84a74d6ebdcc7ad78140bc3b87403fbd28397f
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-02-08 14:53:31 +0100

    feat: add amplify and connect to aws location services

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { Auth } from 'aws-amplify';
+import Location from "aws-sdk/clients/location";
+import awsconfig from './aws-exports';
+import { create } from 'domain';
+
+Amplify.configure(awsconfig);
+
+const createClient = async () => {
+    const credentials = await Auth.currentCredentials();
+    const client = new Location({
+        credentials,
+        region: awsconfig.aws_project_region,
+   });
+   return client;
+}
+
 function App() {
+  const client = createClient;
+  const params = {
+    IndexName: "MyPlaceIndex",
+    Position: [78.6165983, 13.2783941]
+  };
+  client.searchPlaceIndexForPosition(params, (err, data) => {
+    if (err) console.error(err)
+    if (data) console.log(data)
+  })
   return (
     <div className="App">
       <header className="App-header">
