f290ac15c2db30af6fd788fde6eb64bfb248aafc
Lenakh97 <lenaharaldseid@gmail.com>
2022-03-23 15:42:58 +0100

    fix: move options to utils folder to organize

---
+++ src/utils/options.ts
@@ -0,0 +1,30 @@
+export const options = [
+    {
+      name: "Recenter",
+      initial_value: false
+    },
+    {
+      name: "Headingmarker",
+      initial_value: true
+    },
+    {
+      name: "GNSS",
+      initial_value: true
+    },
+    {
+      name: "Singlecell",
+      initial_value: true
+    },
+    {
+      name: "Singlecell history",
+      initial_value: false
+    },
+    {
+      name: "Multicell",
+      initial_value: true
+    },
+    {
+      name: "Multicell locations",
+      initial_value: false
+    }
+  ];
