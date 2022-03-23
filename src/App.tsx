805fdf37f8ed20202ea292aa6be9ea0a599d9790
Lenakh97 <lenaharaldseid@gmail.com>
2022-03-23 15:44:26 +0100

    fix: try to organize and devide into several functions to be able to use checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,167 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { AmplifyProvider, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import Amplify, { Auth } from 'aws-amplify';
-import Location from "aws-sdk/clients/location";
-import { Geo } from "aws-amplify";
-import { create } from 'domain';
-import { createMap, createAmplifyGeocoder } from "maplibre-gl-js-amplify"; 
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect } from 'react';
-import { drawPoints } from "maplibre-gl-js-amplify";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css"; // Optional CSS for Amplify recommended styling
-import awsconfig from './aws-exports';
-import * as turf from "@turf/turf"
-
-const AWS = require("aws-sdk");
-Amplify.configure(awsconfig);
-
-async function initializeMap() {
-  const el = document.createElement("div");
-    el.setAttribute("id", "map");
-    document.body.appendChild(el);
-  const map = await createMap({
-      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-      center: [10.401920000000075, 
-        63.419610000000034], // center in New York
-      zoom: 10,
-  })
-  map.addControl(createAmplifyGeocoder());
-  map.addControl(new maplibregl.NavigationControl());
-  map.on("load", function(){
-    drawPoints("mySourceName", // Arbitrary source name
-        [
-            {
-              coordinates: [10.401920000000076, 
-                63.419610000000034], // [Longitude, Latitude]
-            },/*{
-              coordinates: [10.412930000000075, 
-                63.419610000000034], // [Longitude, Latitude]
-            },
-            {
-              coordinates: [10.423920000000074, 
-                63.419610000000034], // [Longitude, Latitude]
-            },
-            {
-              coordinates: [10.434920000000077, 
-                63.419610000000034], // [Longitude, Latitude]
-            },
-            {
-              coordinates: [10.445920000000078, 
-                63.419610000000034], // [Longitude, Latitude]
-            },
-            {
-              coordinates: [10.456920000000079, 
-                63.419610000000034], // [Longitude, Latitude]
-            },*/
-        ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
-        map,
-        {
-            showCluster: true,
-            unclusteredOptions: {
-                showMarkerPopup: true,
-            },
-            clusterOptions: {
-                showCount: true,
-            },
-        }
-    );
-    //SINGLECELL
-    let _center = turf.point([10.43866796541505, 
-      63.42156251436995]);
-    let _radius = 5;
-    let _options = {
-      steps: 80,
-      units: 'kilometers' // or "mile"
-    };
-
-    let _circle = turf.circle(_center, _radius);
-
-    map.addSource("circleData", {
-          type: "geojson",
-          data: _circle,
-        });
-
-    map.addLayer({
-          id: "circle-fill",
-          type: "fill",
-          source: "circleData",
-          paint: {
-            "fill-color": "rgb(246, 194, 112)",
-            "fill-opacity": 0.4
-          },
-        });
-
-  
-    //MULTICELL
-    let _center2 = turf.point([10.40840864, 
-      63.42123985]);
-    let _radius2 = 2.408;
-    let _options2 = {
-      steps: 80,
-      units: 'kilometers' // or "mile"
-    };
-    let _circle2 = turf.circle(_center2, _radius2);
-
-    map.addSource("circleData2", {
-      type: "geojson",
-      data: _circle2,
-    });
-
-    map.addLayer({
-      id: "circle-fill2",
-      type: "fill",
-      source: "circleData2",
-      paint: {
-        "fill-color": "rgb(229, 99, 153)",
-        "fill-opacity": 0.4,
-      },
-    });
-  })
-
-  Geo.getAvailableMaps();
-  map.setStyle("map0dd2b65d-loginenv");
-  map.resize();
-
-}
-
-async function listDeviceHistory() {
-  const credentials = await Auth.currentCredentials();
-    var location = new AWS.Location({
-      credentials,
-      region: awsconfig.aws_project_region 
-      }); 
-
-  let parameter = {
-    DeviceId: "ExampleDevice-12",
-    TrackerName: "MyTracker"
-  };
-
-  location.getDevicePositionHistory(parameter, (err:Error, data:object) => {
-    if (err) console.log(err);
-    if (data) console.log(data);
-  });
-}
+import { addSingleCell } from './functions/addSingleCell';
+import { addMultiCell } from './functions/addMultiCell';
+import { showDeviceHistory } from './functions/showDeviceHistory';
+import { listDeviceHistory } from './functions/listDeviceHistory';
+import { initializeMap } from './functions/initializeMap';
+import { searchPlaceIndexForPosition } from './functions/searchPlaceIndexForPosition';
+import { reCenterOnPosition } from './functions/reCenterOnPosition';
+import { options } from './utils/options';
 
 function App({signOut, user}: { signOut: (data?: Record<string | number | symbol, any> | undefined) => void; user: {username: string}; }) {
+  const [map, setMap] = useState<Promise<maplibregl.Map>>(initializeMap());
+  const [deviceHistory, setDeviceHistory] = useState<Object>()
+  const [checkedState, setCheckedState] = useState([false, true, true, true, false, true, false]);
+  const multicell = {"lat":63.42123985,"lng":10.40840864,"accuracy":2408};
+  const singlecell = {"lat":63.42156251436995,"lng":10.43866796541505,"accuracy":5000}
   useEffect(() => {
-    const multicell = {"lat":63.42123985,"lng":10.40840864,"accuracy":2408};
-    const singlecell = {"lat":63.42156251436995,"lng":10.43866796541505,"accuracy":5000}
-    const map = initializeMap();
-    const deviceHistory = listDeviceHistory();
+    let deviceHistory = listDeviceHistory();
+    deviceHistory.then(function(result){
+      console.log(result.response)
+      setDeviceHistory(deviceHistory)
+    })
+    console.log(deviceHistory)
+    showDeviceHistory(map)
+    //const map = initializeMap();
+    //setMap(map);
+    const searchPos = searchPlaceIndexForPosition()
+    addSingleCell(map, singlecell.lat, singlecell.lng, singlecell.accuracy)
+    addMultiCell(map, multicell.lat,multicell.lng, multicell.accuracy)
   }, []);
-  return (
+  //const map = initializeMap();
+/*
+  const [checkedState, setCheckedState] = useState(
+    new Array(options.length).fill(false)
+  );*/
+  const handleOnChange = (position:number) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    //recenter
+    if (position == 0 && checkedState[0]==false){
+      reCenterOnPosition(map)
+      console.log("Re-center ~ true")    
+    }
+    if (position == 0 && checkedState[0]==true){
+      //do nothing??
+      console.log("Re-center ~ false")
+    }
+    //headingmarker
+    if (position == 1 && checkedState[1]==false){
+      console.log("Headingmarker ~ true")
+    }
+    if (position == 1 && checkedState[1]==true){
+      console.log("Headingmarker ~ false")
+    }
+    //GNSS
+    if (position == 2 && checkedState[2]==false){
+      console.log("GNSS history ~ true")
+    }
+    if (position == 2 && checkedState[2]==true){
+      console.log("GNSS history ~ false")
+    }
+    //Singlecell
+    if (position == 3 && checkedState[3]==false){
+      addSingleCell(map, singlecell.lat,singlecell.lng, singlecell.accuracy)
+      console.log("Singlecell circle ~ true")
+    }
+    if (position == 3 && checkedState[3]==true){
+      console.log("Singlecell circle ~ false")
+    }
+    //Singlecell location
+    if (position == 4 && checkedState[4]==false){
+      console.log("Singlecell location ~ true")
+    }
+    if (position == 4 && checkedState[4]==true){
+      console.log("Singlecell location ~ false")
+    }
+    //Multicell
+    if (position == 5 && checkedState[5]==false){
+      addMultiCell(map, multicell.lat,multicell.lng, multicell.accuracy)
+      console.log("Multicell circle ~ true")
+    }
+    if (position == 5 && checkedState[5]==true){
+      console.log("Multicell circle ~ false")
+    }
+    //Multicell location
+    if (position == 6 && checkedState[6]==false){
+      console.log("Multicell location ~ true")    }
+    if (position == 6 && checkedState[6]==true){
+      console.log("Multicell location ~ false")    
+    }
+    setCheckedState(updatedCheckedState);
+  }
+ 
+
+    return (
    
         <div className="App">
           <h1> </h1>
                 <div id="map"></div>
                 <h1>Hello {user.username}</h1>
                 <button onClick={signOut}>Sign out</button>
+                {options.map(({ name }, index) => {
+                  return (
+                    <li key={index}>
+                      <div className="toppings-list-item">
+                        <div className="left-section">
+                          <input
+                            type="checkbox"
+                            id={`custom-checkbox-${index}`}
+                            name={name}
+                            value={name}
+                            checked={checkedState[index]}
+                            onChange={() => handleOnChange(index)}
+                          />
+                          <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
         </div>
   );
 }
