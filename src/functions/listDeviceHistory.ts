4962fa4cbad5beed8f8db181adf5e9770fa66e4e
Lenakh97 <lenaharaldseid@gmail.com>
2022-05-05 14:19:37 +0200

    fix: fix promise handeling to get DevicePositions, add error handelin with try/catch

--- src/functions/listDeviceHistory.ts
+++ src/functions/listDeviceHistory.ts
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
-import '../App.css';
-import { AmplifyProvider, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import Amplify, { Auth } from 'aws-amplify';
-import { Geo } from "aws-amplify";
-import { createMap, createAmplifyGeocoder } from "maplibre-gl-js-amplify"; 
+import "../App.css";
+import "@aws-amplify/ui-react/styles.css";
+import Amplify, { Auth } from "aws-amplify";
 import "maplibre-gl/dist/maplibre-gl.css";
-import { useEffect } from 'react';
-import { drawPoints } from "maplibre-gl-js-amplify";
-import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css"; // Optional CSS for Amplify recommended styling
-import awsconfig from '../aws-exports';
-
+import awsconfig from "../aws-exports";
 
 Amplify.configure(awsconfig);
 const AWS = require("aws-sdk");
 export async function listDeviceHistory() {
-    const credentials = await Auth.currentCredentials();
-      var location = new AWS.Location({
-        credentials,
-        region: awsconfig.aws_project_region 
-        }); 
-  
-    let parameter = {
-      DeviceId: "ExampleDevice-12",
-      TrackerName: "MyTracker"
-    };
-  
-    let data = await location.getDevicePositionHistory(parameter, (err:Error, data:object) => {
-      if (err) console.log(err);
-      if (data) console.log(data);
-    });
-    return data;
+  const credentials = await Auth.currentCredentials();
+  var location = new AWS.Location({
+    credentials,
+    region: awsconfig.aws_project_region,
+  });
+
+  let parameter = {
+    DeviceId: "ExampleDevice-23",
+    TrackerName: "MyTracker",
+    StartTimeInclusive: new Date(
+      Date.now() - 48 * 60 * 60 * 1000
+    ).toISOString(),
+  };
+
+  try {
+    const data = await location.getDevicePositionHistory(parameter).promise();
+    return data.DevicePositions;
+  } catch (err) {
+    console.error(err);
+    return [];
   }
+}
